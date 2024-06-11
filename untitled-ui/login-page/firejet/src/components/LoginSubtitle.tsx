eba828299959c21b0f402c476800e1571d1c4d81
StephenAlvin <stephenalvin0912@gmail.com>
2024-06-11 18:12:12 +0800

    fixed eslint errors

--- untitled-ui/login-page/firejet/src/components/LoginSubtitle.tsx
+++ untitled-ui/login-page/firejet/src/components/LoginSubtitle.tsx
@@ -1,10 +1,10 @@
 export default function LoginSubtitle({ className = "" }: LoginSubtitleProps) {
   return (
-    <heading
+    <h1
       className={`self-stretch pt-1.5 text-center leading-6 text-[#475467] ${className}`}
     >
       Welcome back! Please enter your details.
-    </heading>
+    </h1>
   );
 }
 
--- untitled-ui/login-page/firejet/src/components/LoginTitle.tsx
+++ untitled-ui/login-page/firejet/src/components/LoginTitle.tsx
@@ -1,10 +1,10 @@
 export default function LoginTitle({ className = "" }: LoginTitleProps) {
   return (
-    <heading
+    <h1
       className={`self-stretch pt-[18px] text-center text-3xl font-semibold leading-[38px] text-[#101828] ${className}`}
     >
       Log in to your account
-    </heading>
+    </h1>
   );
 }
 
